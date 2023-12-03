68ca8cb2466d0952531281d94e428aa97dcfc4f0
Jay Lyon <jay@jaylyonvt.com>
2023-12-03 05:56:50 -0500

    Added Day03

--- Program.cs
+++ Program.cs
@@ -11,11 +11,18 @@ public class Program
         Console.WriteLine($"Day 01 Part 1 = {d1.Part1()} ({sw.Elapsed.Microseconds} µs)");
         sw.Restart();
         Console.WriteLine($"Day 01 Part 2 = {d1.Part2()} ({sw.Elapsed.Microseconds} µs)");
+        
         var d2 = new Day02();
         sw.Restart();
         Console.WriteLine($"Day 02 Part 1 = {d2.Part1()} ({sw.Elapsed.Microseconds} µs)");
         sw.Restart();
         Console.WriteLine($"Day 02 Part 2 = {d2.Part2()} ({sw.Elapsed.Microseconds} µs)");
+        
+        var d3 = new Day03();
+        sw.Restart();
+        Console.WriteLine($"Day 03 Part 1 = {d3.Part1()} ({sw.Elapsed.Microseconds} µs)");
+        sw.Restart();
+        Console.WriteLine($"Day 03 Part 2 = {d3.Part2()} ({sw.Elapsed.Microseconds} µs)");
         sw.Stop();
     }
 }
