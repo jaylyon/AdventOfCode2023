2f591f31a64de8bdf6b7f4161963336176c47f6f
Jay Lyon <jay@jaylyonvt.com>
2023-12-15 07:00:13 -0500

    Added Day15

--- Day15.cs
+++ Day15.cs
@@ -1,32 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
-
-namespace AOC2023
+﻿namespace AOC2023
 {
     internal class Day15
     {
-        private const string sample = @"";
+        private readonly string input = File.ReadAllText(@"Input\15.txt");
 
-        public Day15()
+        private static int Hash(string s)
         {
-            var input = sample;
-            //input = File.ReadAllText(@"Input\15.txt");
-
+            var hash = 0;
+            foreach (var c in s.Where(c => c != '\n'))
+            {
+                hash += (int)c;
+                hash *= 17;
+                hash %= 256;
+            }
+            return hash;
         }
 
         internal int Part1()
         {
-            return 0;
+            return input.Split(',').Sum(Hash);
         }
 
         internal int Part2()
         {
-            return 0;
+            var boxes = Enumerable.Range(0, 256).Select(_ => new Box()).ToArray();
+            foreach (var s in input.Split(","))
+            {
+                var values = s.Split('-', '=');
+                var label = values[0];
+                var box = boxes[Hash(label)];
+
+                if (s.Contains('-'))
+                    box.Lenses.RemoveAll(l => l.Label == label);
+                else
+                {
+                    var f = int.Parse(values[1]);
+                    var lens = box.Lenses.FirstOrDefault(b => b.Label == label);
+                    if (lens != null)
+                        lens.FocalLength = f;
+                    else
+                        box.Lenses.Add(new Lens() { Label = label, FocalLength = f });
+                }
+            }
+
+            return boxes.Select((t, i) => t.Lenses.Sum(lens => (i + 1) * (t.Lenses.IndexOf(lens) + 1) * lens.FocalLength)).Sum();
         }
 
+        private class Box
+        {
+            public readonly List<Lens> Lenses = new();
+        }
+
+        private class Lens
+        {
+            public string? Label { get; init; }
+            public int FocalLength { get; set; }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,105 +5,160 @@ public class Program
 {
     public static void Main()
     {
-        var d1 = new Day01();
+        int today = DateTime.Today.Day;
+
         Stopwatch sw = new Stopwatch();
-        Console.WriteLine("Day 01");
-        sw.Start();
-        Console.WriteLine($"    Part 1 = {d1.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d1.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
-        
-        var d2 = new Day02();
-        Console.WriteLine("Day 02");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d2.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d2.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
-        
-        var d3 = new Day03();
-        Console.WriteLine("Day 03");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d3.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d3.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
 
-        var d4 = new Day04();
-        Console.WriteLine("Day 04");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d4.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d4.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 1)
+        {
+            var d1 = new Day01();
+            Console.WriteLine("Day 01");
+            sw.Start();
+            Console.WriteLine($"    Part 1 = {d1.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d1.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
-        var d5 = new Day05p2();
-        var d5bf = new Day05p1();
-        Console.WriteLine("Day 05");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d5bf.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d5.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 2)
+        {
+            var d2 = new Day02();
+            Console.WriteLine("Day 02");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d2.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d2.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
-        var d6 = new Day06();
-        Console.WriteLine("Day 06");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d6.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d6.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 3)
+        {
+            var d3 = new Day03();
+            Console.WriteLine("Day 03");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d3.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d3.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
-        var d7 = new Day07();
-        Console.WriteLine("Day 07");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d7.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d7.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 4)
+        {
+            var d4 = new Day04();
+            Console.WriteLine("Day 04");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d4.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d4.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
-        var d8 = new Day08();
-        Console.WriteLine("Day 08");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d8.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d8.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 5)
+        {
+            var d5 = new Day05p2();
+            var d5bf = new Day05p1();
+            Console.WriteLine("Day 05");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d5bf.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d5.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
-        var d9 = new Day09();
-        Console.WriteLine("Day 09");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d9.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d9.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 6)
+        {
+            var d6 = new Day06();
+            Console.WriteLine("Day 06");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d6.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d6.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
-        var d10 = new Day10();
-        Console.WriteLine("Day 10");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d10.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d10.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 7)
+        {
+            var d7 = new Day07();
+            Console.WriteLine("Day 07");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d7.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d7.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
-        var d11 = new Day11();
-        Console.WriteLine("Day 11");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d11.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d11.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 1)
+        {
+            var d8 = new Day08();
+            Console.WriteLine("Day 08");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d8.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d8.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
-        var d12 = new Day12();
-        Console.WriteLine("Day 12");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d12.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d12.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 9)
+        {
+            var d9 = new Day09();
+            Console.WriteLine("Day 09");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d9.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d9.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
-        var d13 = new Day13();
-        Console.WriteLine("Day 13");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d13.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d13.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 10)
+        {
+            var d10 = new Day10();
+            Console.WriteLine("Day 10");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d10.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d10.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
-        var d14 = new Day14();
-        Console.WriteLine("Day 14");
-        sw.Restart();
-        Console.WriteLine($"    Part 1 = {d14.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
-        sw.Restart();
-        Console.WriteLine($"    Part 2 = {d14.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        if (today == 11)
+        {
+            var d11 = new Day11();
+            Console.WriteLine("Day 11");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d11.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d11.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
+
+        if (today == 12)
+        {
+            var d12 = new Day12();
+            Console.WriteLine("Day 12");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d12.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d12.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
+
+        if (today == 13)
+        {
+            var d13 = new Day13();
+            Console.WriteLine("Day 13");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d13.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d13.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
+
+        if (today == 14)
+        {
+            var d14 = new Day14();
+            Console.WriteLine("Day 14");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d14.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d14.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
+
+        if (today == 15)
+        {
+            var d15 = new Day15();
+            Console.WriteLine("Day 15");
+            sw.Restart();
+            Console.WriteLine($"    Part 1 = {d15.Part1()} ({sw.Elapsed.TotalMilliseconds} ms)");
+            sw.Restart();
+            Console.WriteLine($"    Part 2 = {d15.Part2()} ({sw.Elapsed.TotalMilliseconds} ms)\n");
+        }
 
         sw.Stop();
     }
